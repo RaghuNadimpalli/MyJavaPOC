885380ff28b318b2bb8c0447ead9ba7f6cb5f0ae
Raghu Nadimpalli <varma2801@gmail.com>
2015-06-25 21:28:07 -0400

    JavaMailPOC

---
+++ MyJavaMailPOC/src/main/java/com/nadimpalli/MyJavaMailPOC/App.java
@@ -0,0 +1,13 @@
+package com.nadimpalli.MyJavaMailPOC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
